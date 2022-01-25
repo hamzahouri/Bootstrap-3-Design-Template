5aeda9f10b8d8af40f6dfa96951916544c000ef7
unknown <hamzahouri@gmail.com>
2022-01-25 18:35:17 +0100

    improving the tab section

--- js/vendor/main.js
+++ js/vendor/main.js
@@ -8,4 +8,13 @@ $(function () {
             navbar.removeClass('scrolled');
         }
     });
+    //Deal with tabs
+    $('.tab-switch li').click(function () {
+        //Add selected class to active link
+        $(this).addClass('selected').siblings().removeClass('selected');
+        // Hide all divs
+        $('.tabs-section .tabs-content > div').hide();
+        //show div connected to link
+        $('.' +$(this).data('class')).show(); 
+    })
 });
